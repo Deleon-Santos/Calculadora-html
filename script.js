914425506df9e6a5d682fc167bb283937d94479f
Deleon <delps.santos1987@gmail.com>
2024-06-09 01:22:40 -0300

    testando transições

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 function displaynum(n1){
     calculadora.display . value  = calculadora.display.value + n1;
 }
+
+function apagarUltimoNumero() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function toggleSign() {
+    const display = document.getElementById('display');
+    if (display.value.charAt(0) === '-') {
+        display.value = display.value.slice(1);
+    } else {
+        display.value = '-' + display.value;
+    }
+}
+function calculateSqrt() {
+    const display = document.getElementById('display');
+    
+    
+    try {
+        display.value = Math.sqrt(eval(display.value));
+    } 
+ 
+    catch (e) {
+        display.value = 'Error';
+    }
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } 
+  
+    catch (e) {
+        display.value = 'Error';
+    }
+}
